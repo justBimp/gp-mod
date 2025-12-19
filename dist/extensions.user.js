f77a02e2edbd8027b11da0f12d2d9e1e933496bd
justBimp <lafisteamx0@gmail.com>
2025-12-19 15:38:11 +0300

    Update extensions.user.js

--- dist/extensions.user.js
+++ dist/extensions.user.js
@@ -1,1 +1,13 @@
+// ==UserScript==
+// @name         GarticPhone Mod - Extensions
+// @namespace    https://github.com/justBimp/gp-mod
+// @version      1.0.0
+// @author       justBimp
+// @match        https://garticphone.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=garticphone.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// ==/UserScript==
 
+console.log('GarticPhone Mod loaded successfully!');
+alert('Hello from justBimp Mod! 🚀');
